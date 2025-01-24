fe5adec66679b4b7e4a399fc7033630262e9e857
youto023 <yuuto9036@gmail.com>
2025-01-24 10:37:57 +0900

    01_01完成

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,42 +1,50 @@
-#include"object3d.hlsli"
-struct Pixelshaderoutput
-{
-    float4 color : SV_TARGET0;
+#include "object3d.hlsli"
 
+struct Material{
+    float32_t4 color;
+    int32_t enableLighting;
 };
-Texture2D<float4> gTexture : register(t0);
-SamplerState gSampler : register(s0);
-struct Material
-{
-    float4 color;
-};
+
+//struct DirectionalLight{
+//    float32_t4 color;//!<ライトの色
+//    float32_t3 direction;//!<ライトの向き
+//    float intensity;//!<輝度
+//};
+
 ConstantBuffer<Material> gMaterial : register(b0);
+//ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+struct PixelShaderOutput{
+    float32_t4 color : SV_TARGET0;
+};
 
 
-
-
-Pixelshaderoutput main(VertexShaderOutput input)
-{
-    Pixelshaderoutput output;
-    output.color = gMaterial.color; //float32_t4(1.0, 1.0, 1.0, 1.0);
-    float4 textureColor = gTexture.Sample(gSampler, input.texcoord);
-    output.color = gMaterial.color * textureColor;
-	
-	//output.colorのa値が0の時にPixelを棄却
+PixelShaderOutput main(VertexShaderOutput input){
+    PixelShaderOutput output;
+    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+    output.color = gMaterial.color*textureColor;
+    //if (gMaterial.enableLighting != 0){
+    //    float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+    //    output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+    //}
+    //else{//Lightingしない場合。前回までと同じ演算
+    //    output.color = gMaterial.color * textureColor;
+    //}
+    
+    //textureのα値が0.5以下の時にPixelを棄却
+    if (textureColor.a <= 0.5){
+        discard;
+    }
+    //textureのα値が0の時にPixelを棄却
+    if (textureColor.a == 0.0){
+        discard;
+    }
+    //output.colorのα値が0の時にPixelを棄却
     if (output.color.a == 0.0)
     {
         discard;
     }
-	
-	
-	
+    
     return output;
 }
-
-
-
-
-
-
-
-
--- Transform.h
+++ Transform.h
@@ -1,7 +1,19 @@
 #pragma once
-#include"Vector3.h"
-struct Transform {
-    Vector3 scale;
-    Vector3 rotate;
-    Vector3 translate;
+#include"math/Vector3.h"
+struct Transform{
+	Vector3 scale;
+	Vector3 rotate;
+	Vector3 translate;
+};
+
+struct TransformationMatrix {
+	Matrix4x4 WVP;
+	Matrix4x4 World;
+};
+
+struct Particle {
+	Transform transform;
+	Vector3 velocity;
+	float currentTime;
+	//Vector4 color;
 };
